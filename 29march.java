127eddf82a22bc87d66ac42ed1825775a00ccc7d
Yuvraj singh <136175293+YuvrajSinghRathoreJi@users.noreply.github.com>
2024-03-29 10:32:05 +0530

    29march.java

---
+++ 29march.java
@@ -0,0 +1,70 @@
+ 1)
+public class Yuv {
+
+    public static void main(String[] args) {
+        System.out.println("india");
+        System.out.println("indonesia");
+        System.out.println("iran");
+        System.out.println("china");
+        System.out.println("fiji");
+    }
+}
+
+———————————————————————————
+
+2)
+public class Yuv {
+
+    public static void main(String[] args) {
+        System.out.println("goa");
+        System.out.println("assam");
+        System.out.println("bihar");
+        System.out.println("panjab");
+        System.out.println("haryana");
+    }
+}
+
+———————————————————————
+
+
+3)
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("pizza");
+        System.out.println("Salad");
+        System.out.println("kaju katli");
+        System.out.println("Sandwich");
+        System.out.println("Rice");
+    }
+}
+
+————————————————————
+4)
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("vaishu");
+        System.out.println("pankaj");
+        System.out.println("vikram");
+        System.out.println("sanju");
+        System.out.println("yuvraj");
+    }
+}
+
+
+—————————————————————
+5)
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("apple ");
+        System.out.println("banana");
+        System.out.println("orange");
+        System.out.println("mango");
+        System.out.println("kiwi");
+    }
+}
+
